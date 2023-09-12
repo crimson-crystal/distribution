b9182956bc8a1f733a83286c22480ffb4b3e764b
devnote-dev <devnote.dev75@gmail.com>
2023-09-12 12:23:07 +0100

    chore(homebrew): update url to head

--- Formula/crimson.rb
+++ Formula/crimson.rb
@@ -2,7 +2,8 @@ class Crimson < Formula
     desc "A Crystal Version Manager"
     homepage "https://github.com/crimson-crystal/crimson#readme"
     license "MPL-2.0"
-    url "https://github.com/crimson-crystal/crimson/archive/nightly.tar.gz"
+    # url "https://github.com/crimson-crystal/crimson/archive/0.1.0.tar.gz"
+    url "https://github.com/crimson-crystal/crimson/archive/refs/heads/main.zip"
     sha256 "17ed776dc4109ec29353527fbb89d18b5b13e2ebf94ccdf8dd3ab295c2018fc1"
     version '0.1.0'
 
