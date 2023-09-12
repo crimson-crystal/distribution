6f81b84045853747edc12b7670982e3994bcba19
devnote-dev <devnote.dev75@gmail.com>
2023-09-12 02:01:12 +0100

    fix(homebrew): specify explicit version

--- Formula/crimson.rb
+++ Formula/crimson.rb
@@ -4,6 +4,7 @@ class Crimson < Formula
     license "MPL-2.0"
     url "https://github.com/crimson-crystal/crimson/archive/nightly.tar.gz"
     sha256 "17ed776dc4109ec29353527fbb89d18b5b13e2ebf94ccdf8dd3ab295c2018fc1"
+    version '0.1.0'
 
     depends_on "crystal" => :build
     conflicts_with "crystal", because: "homebrew install overrides crimson installs"
