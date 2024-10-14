744d9f72b965de9c6a3040017ca52b55df45f691
devnote-dev <devnote.dev75@gmail.com>
2024-10-14 18:08:58 +0100

    feat: add update helper script

---
+++ update.py
@@ -0,0 +1,54 @@
+'''update.py'''
+import sys
+import hashlib
+from http.client import HTTPSConnection
+import json
+
+
+def update_bucket(version: str) -> None:
+    '''simple json loading and dumping'''
+
+    content: dict
+    with open('./bucket/crimson.json', encoding='utf8', mode='r') as file:
+        content = json.load(file)
+        content['version'] = version
+        content['url'] = [
+            'https://github.com/crimson-crystal/crimson/releases/download/'
+            f'{version}/crimson-{version}-windows-x86_64-msvc.zip'
+        ]
+
+    with open('./bucket/crimson.json', encoding='utf8', mode='w') as file:
+        json.dump(content, file, indent=4)
+
+
+def update_formula(version: str) -> None:
+    '''essentially replaces the line rather than trying to do magic stuff'''
+
+    conn = HTTPSConnection('github.com')
+    conn.request('GET',
+                 'https://github.com/crimson-crystal/crimson/'
+                 f'archive/refs/tags/{version}.tar.gz')
+
+    sha256: str
+    with conn.getresponse() as res:
+        sha256 = hashlib.sha256(res.read()).hexdigest()
+
+    lines: list
+    with open('./Formula/crimson.rb', encoding='utf8', mode='r') as file:
+        lines = file.readlines()
+        lines[4] = '    url "https://github.com/crimson-crystal/crimson/' \
+                   f'archive/refs/tags/{version}.tar.gz"\n'
+        lines[5] = f'    sha256 "{sha256}"\n'
+        lines[6] = f"    version '{version}'\n"
+
+    with open('./Formula/crimson.rb', encoding='utf8', mode='w') as file:
+        file.write(''.join(lines))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('no version specified')
+        sys.exit(1)
+
+    update_bucket(sys.argv[1])
+    update_formula(sys.argv[1])
